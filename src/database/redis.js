1038e36012ccb6708899858f07115bde7ec4f8fc
Jaesuk <jaesuk95@outlook.com>
2023-03-24 18:37:26 +0900

    rabbitMQ Connection with redis Client connected (listener positioned at server.js)

--- src/database/redis.js
+++ src/database/redis.js
@@ -5,7 +5,7 @@ const redisClient = redis.createClient({
         host: `${process.env.REDIS_HOST}`,
         port: `${process.env.REDIS_PORT}`
     },
-    // password: `${process.env.REDIS_PASSWORD}`
+    password: `${process.env.REDIS_PASSWORD}`
 });
 
 (async () => {  // redis connect
